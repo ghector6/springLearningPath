4f6db955a138880a4885bb707849f9b3707bd642
Hector Garcia Rosales <ghectormus@gmail.com>
2022-09-12 09:16:50 -0500

    First RESTService with spring

---
+++ src/main/groovy/com/example/restservice/RestServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.restservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RestServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication, args)
+	}
+
+}
